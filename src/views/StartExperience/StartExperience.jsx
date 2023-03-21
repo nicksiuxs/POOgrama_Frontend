e00c530fa4c71c570a479584ea35992370475854
Nicolás Londoño <nicolas.londono@veevart.com>
2023-03-21 12:25:02 -0500

    Add: start expirience view

--- src/views/StartExperience/StartExperience.jsx
+++ src/views/StartExperience/StartExperience.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import logo from "../../assets/logo.png"
+import play from "../../assets/play.png"
+
+import "./StartExperience.css"
 
 const StartExperience = () => {
+
     return (
-        <div>StartExperience</div>
+        <main class="start-experience">
+            <section>
+                <figure>
+                    <img src={logo} alt="Poograma" />
+                </figure>
+            </section>
+            <section>
+                <button className='button-play'>
+                    <img src={play} alt="play" />
+                </button>
+            </section>
+        </main>
     )
 }
 
