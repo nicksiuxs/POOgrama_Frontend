68cc530a2aeb358da62b3e99b9089d86e38fee08
Nicolás Londoño <nicolas.londono@veevart.com>
2023-03-27 22:20:16 -0500

    Add: level 01 example

--- src/views/Level1/Level1.jsx
+++ src/views/Level1/Level1.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import LeftSide from '../../components/LeftSide/LeftSide';
+// import ⁄logo from "../../assets/logo.png"
+import exampleImage from "../../assets/level-01.png"
+
+const levelInformation = {
+    title: "Clase",
+    definition: "En esta sección veremos el concepto de clase. Las clases son la plantilla de una entidad, es el modelo que nos ayuda a partir de esa plantilla, a crear nuestros objetos de esa clase.",
+    example: "Para brindarte una mejor ayuda, puedes observar el siguiente ejemplo.",
+    imageExample: exampleImage,
+    exampleDescription: "En el ejemplo vemos a la izquierda la clase persona, la plantilla de la entidad. A la derecha observamos 3 entes u objetos creados a partir de esta clase.",
+    activity: "Para esta primera actividad, deberás arrastrar los objetos que pertenezcan a la clase dada dentro del recuadro marcado."
+}
 
 const Level1 = () => {
     return (
-        <div>
-            <h1>Nivel 1</h1>
-        </div>
+        <main>
+            <LeftSide {...levelInformation} />
+        </main>
     )
 }
 
