a4513dfa138fbaa47a44401ce236c5ebbd449997
Nicolás Londoño <nicolas.londono@veevart.com>
2023-03-27 23:30:08 -0500

    checkpoint

---
+++ src/components/CardItem/CardItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardItem = () => {
+    return (
+        <div className='card-container'>
+            <p>content</p>
+        </div>
+    )
+}
+
+export default CardItem
---
+++ src/components/ContainerCards/ContainerCards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ContainerCards = ({ status }) => {
+    return (
+
+        <div className="layout-cards" >
+            <p>{status} hero</p>
+            {/* Cards */}
+        </div>
+
+    )
+}
+
+export default ContainerCards
