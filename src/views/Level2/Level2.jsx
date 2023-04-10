2127b253d99f9e4a116432757c18246a96b22cd7
Nicolás Londoño <nicolas.londono@veevart.com>
2023-04-10 18:37:31 -0500

    fix: problem

--- src/views/Level2/Level2.jsx
+++ src/views/Level2/Level2.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import LeftSide from '../../components/LeftSide/LeftSide';
 import './Level2.css'
